0725a1e315cd33dd4069d6bf66fc2b4354049e9a
Jesus bossa <jesusbossa@protonmail.com>
2018-05-22 11:48:26 -0500

    Security fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCqIevmIglQjm3ZPW4XgJ5lbs7frjS_3hM",
+    authDomain: "collegepw-task.firebaseapp.com",
+    databaseURL: "https://collegepw-task.firebaseio.com",
+    projectId: "collegepw-task",
+    storageBucket: "collegepw-task.appspot.com",
+    messagingSenderId: "551254640439"
+  }
 };
